e07f4421b52d234fed90a19bd5c6b0f13b8257a1
Lucas A Jones <Necxii@gmail.com>
2016-03-08 14:33:22 +0000

    GB now uses correct memory space (Haxe Bug)

--- src/GB.hx
+++ src/GB.hx
@@ -4,13 +4,13 @@ class GB
 {
 	public static function main():Void
 	{
-		var cpu = new CPU(new Memory(256, BIOS.DMG));
+		var cpu = new CPU(new Memory(0xFFFF, BIOS.DMG));
 
 		//untyped setInterval(cpu.cycle, 0);
 
 		while(true)
 		{
 			cpu.cycle();
-		}		
+		}
 	}
 }
